e16dd7e627aea1461d9ce0178e7872c11052e1cb
markmwaura <markkmwaura@gmail.com>
2016-07-29 18:14:09 +0300

    add tribonacci

---
+++ fib.py
@@ -0,0 +1,7 @@
+import fibonacci
+
+
+ # Now call the function we just defined:
+
+num = int(raw_input("Enter a value here"))
+print tribonacci.trib(num)
---
+++ tribonacci.py
@@ -0,0 +1,9 @@
+def trib(n):   # return Fibonacci series up to n
+    result = []
+    a, b, c = 0, 0, 1
+
+    while b < n:
+        result.append(b)
+        a, b, c = b, c, a + b + c
+
+    return result
