7eb20ec60da006009ffb612ae36f957beddc9795
markmwaura <markkmwaura@gmail.com>
2016-08-02 12:05:41 +0300

    add both sort and reverse sort

---
+++ sortnames/sortfile.py
@@ -0,0 +1,10 @@
+import json
+
+with open("MOCK_DATA.json") as json_file:
+    json_data = json.load(json_file)
+    #print(json_data)
+    
+    for k in sorted(json_data):	
+	print k["first_name"]
+
+
